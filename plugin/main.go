4adba9891b4844d5272db6f936545e6edb641ffd
Tim Routowicz <troutowicz@gmail.com>
2025-07-04 20:44:57 -0400

    Fix import

--- plugin/main.go
+++ plugin/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"golang.org/x/tools/go/analysis"
 
-	"configlinter"
+	"github.com/troutowicz/configlinter"
 )
 
 // New is the entry point for the golangci-lint Go plugin system
